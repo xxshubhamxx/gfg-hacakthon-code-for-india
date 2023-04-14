4416e44e633b962a1edc34cbbc17c9a9e7c1bffb
xxshubhamxx <shubham2002garg@gmail.com>
2023-04-15 01:18:45 +0530

    refactored code to use local pkl file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import requests
 import os
 from openai_files import FitnessGenie
+from ml_model import model
 
 app = Flask(__name__)
 
@@ -10,39 +11,39 @@ results = ""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', getres = getres)
+    return render_template('index.html')
 
-def getres(data1):
-    print("Input data:",data1)
-    data = {
-        "instances": [
-            {
-                "step_count": data1['step_count'],
-                "calories_burnt": data1['calories_burnt'],
-                "hours_of_sleep": data1['hours_of_sleep'],
-                "Weight": data1['Weight'],
-                "Height": data1['Height'],
-                "calories": data1['calories'],
-                "BMI": data1['BMI']
-            }
-        ]
-    }
+# def getres(data1):
+#     print("Input data:",data1)
+#     data = {
+#         "instances": [
+#             {
+#                 "step_count": data1['step_count'],
+#                 "calories_burnt": data1['calories_burnt'],
+#                 "hours_of_sleep": data1['hours_of_sleep'],
+#                 "Weight": data1['Weight'],
+#                 "Height": data1['Height'],
+#                 "calories": data1['calories'],
+#                 "BMI": data1['BMI']
+#             }
+#         ]
+#     }
 
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    response = requests.post('https://gcp-model-service-emffglsbhq-uc.a.run.app/predict', data=json.dumps(data), headers=headers)
-    prediction = response.json()
-    tempresult = jsonify(prediction)
-    result = tempresult.get_json()['predictions'][0]
-    print("AutoML Model Output: ",result)
-    sc = result['scores']
-    r = {}
-    if sc[0] > sc[1]:
-        r['class'] = 'Unhealthy'
-        r['score'] = result['scores'][0]
-    else:
-        r['class'] = 'Healthy'
-        r['score'] = result['scores'][1]
-    return r
+#     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+#     response = requests.post('https://gcp-model-service-emffglsbhq-uc.a.run.app/predict', data=json.dumps(data), headers=headers)
+#     prediction = response.json()
+#     tempresult = jsonify(prediction)
+#     result = tempresult.get_json()['predictions'][0]
+#     print("AutoML Model Output: ",result)
+#     sc = result['scores']
+#     r = {}
+#     if sc[0] > sc[1]:
+#         r['class'] = 'Unhealthy'
+#         r['score'] = result['scores'][0]
+#     else:
+#         r['class'] = 'Healthy'
+#         r['score'] = result['scores'][1]
+#     return r
 
 @app.route('/res')
 def res():    
@@ -52,9 +53,9 @@ def res():
     h = float(data['Height'])
     data['BMI'] = str(w / (h * h / 10000))
     data['Calorie_Deficient_or_Over'] = str(float(data['calories']) - float(data['calories_burnt']) - 2000)
-    pred = getres(data)
+    pred = model.predict(data)
     fitness_plan = FitnessGenie.ai_response(json.dumps(data), str(pred))
     return render_template('res.html', results=pred, plan=fitness_plan, billing_info=FitnessGenie.billing_resp())
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=5673, debug=True)
